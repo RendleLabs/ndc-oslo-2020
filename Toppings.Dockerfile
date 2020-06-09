51f059b15206ba982fab7e65c341e7b93d012074
Mark Rendle <mark@rendle.io>
2020-06-09 10:25:34 +0100

    Get it right Mark

---
+++ Toppings.Dockerfile
@@ -0,0 +1,32 @@
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build
+
+WORKDIR /src
+
+# copy csproj and restore as distinct layers
+COPY ./Pizza.sln .
+COPY ./src/Frontend/Frontend.csproj ./src/Frontend/
+COPY ./src/JaegerTracing/JaegerTracing.csproj ./src/JaegerTracing/
+COPY ./src/Orders/Orders.csproj ./src/Orders/
+COPY ./src/Orders.PubSub/Orders.PubSub.csproj ./src/Orders.PubSub/
+COPY ./src/Orders.ShopConsole/Orders.ShopConsole.csproj ./src/Orders.ShopConsole/
+COPY ./src/Toppings/Toppings.csproj ./src/Toppings/
+COPY ./src/Toppings.Data/Toppings.Data.csproj ./src/Toppings.Data/
+COPY ./test/TestHelpers/TestHelpers.csproj ./test/TestHelpers/
+COPY ./test/Toppings.Tests/Toppings.Tests.csproj ./test/Toppings.Tests/
+
+RUN dotnet restore
+
+# copy and publish app and libraries
+COPY . .
+
+RUN dotnet build -c Release --no-restore
+RUN dotnet test -c Release --no-build
+RUN dotnet publish src/Toppings -c Release -o /app --no-build
+
+# final stage/image
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1
+
+WORKDIR /app
+COPY --from=build /app .
+
+ENTRYPOINT ["./Toppings"]
---
+++ src/JaegerTracing/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTracing.Util;
+
+namespace JaegerTracing
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddJaegerTracing(this IServiceCollection services)
+        {
+            var jaeger = Environment.GetEnvironmentVariable("JAEGER_SERVICE_NAME");
+            if (!string.IsNullOrEmpty(jaeger))
+            {
+                services.AddOpenTracing();
+                services.AddSingleton(provider =>
+                {
+                    var loggerFactory = provider.GetRequiredService<ILoggerFactory>();
+
+                    var config = Jaeger.Configuration.FromEnv(loggerFactory);
+                    var tracer = config.GetTracer();
+
+                    if (!GlobalTracer.IsRegistered())
+                    {
+                        GlobalTracer.Register(tracer);
+                    }
+
+                    return tracer;
+                });
+            }
+
+            return services;
+        }
+    }
+}
